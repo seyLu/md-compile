6368b1fa00b9091a20ce629d7d80e0bfa34b3dbb
seylu <98249191+seyLu@users.noreply.github.com>
2023-06-11 03:48:14 +0800

    add theme switch transition

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
-const themeToggler = document.getElementById("theme-toggler")
-const theme = document.body.dataset.theme
+const themeToggler = document.getElementById("theme-toggler");
+const bodyDataset = document.body.dataset;
+
+const githubMarkdownCSS = document.getElementById("github-markdown-css");
+const pathToCSS = "/static/css"
 
 themeToggler.addEventListener("change", () => {
-   if (theme === "light") theme = "dark";
-   else theme = "light";
+   if (bodyDataset.theme === "light") {
+      bodyDataset.theme = "dark";
+      githubMarkdownCSS.href = `${pathToCSS}/github-markdown-dark.min.css`;
+   } else {
+      bodyDataset.theme = "light";
+      githubMarkdownCSS.href = `${pathToCSS}/github-markdown-light.min.css`;
+   }
 })
